35c09847bd20ff9efda8e776011c55b48bc6b693
Marcin <marcin.orent@gmail.com>
2018-10-17 10:23:34 +0200

    Ukończyłem lab1

--- Lab1/scripts/main.js
+++ Lab1/scripts/main.js
@@ -1,2 +1,20 @@
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+myHeading.textContent = 'Witaj podróżniku!';
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+    var myName = prompt('Podaj nazwę.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Witaj ponownie, ' + myName;
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Witaj ponownie, ' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+  }
+  
