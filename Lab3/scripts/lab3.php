b39b6157a75b3276b9c3908efad8951c31619113
Marcin <marcin.orent@gmail.com>
2018-11-06 21:33:29 +0100

    Wykonałem lab3. Stworzylem prosta baze danych w php my admin i polaczylem ja z moja strona. Utworzylem proste elementy i zapisalem ss z wywolania bazy

---
+++ Lab3/scripts/lab3.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db";
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$conn->set_charset("utf8");
+$sql="select * from postacie ";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo  '<h1>' . $row["Vegeta"]."</h1>" . $row["Goku"]."<hr>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+?>
